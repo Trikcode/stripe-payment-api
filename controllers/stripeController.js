b842c15a1880aef933dda742dab63399d5711dea
Nobert Patrick <73602565+Trikcode@users.noreply.github.com>
2024-03-06 18:09:06 +0300

    code updates

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -2,14 +2,19 @@ const stripe = require('stripe')(process.env.STRIPE_KEY)
 
 const stripeController = async (req, res) => {
   const { cartItems } = req.body
+  let total = 0
 
-  const calculateOrderAmount = (items) => {
-    return items.reduce((acc, item) => acc + item.price * item.amount, 0)
+  const calculateOrderAmount = () => {
+    return cartItems.reduce((acc, item) => acc + item.price * item.amount, 0)
   }
 
+  total = calculateOrderAmount(cartItems)
+
+  total = total * 100
+
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(cartItems),
-    currency: 'usd',
+    amount: total,
+    currency: 'GBP',
   })
 
   res.json({ clientSecret: paymentIntent.client_secret })
